ca93ffcfac32dc131cec8c7cb77f5e6ef3e99d80
Faisal <mfaisalhidayatullah@gmail.com>
2021-11-18 14:25:39 +0700

    slicing projects page

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,3 +28,45 @@ function setDarkMode() {
 	}
 	document.getElementById('dark-mode').className = darkIcon;
 }
+
+// Owl Carousel
+$(document).ready(function () {
+	initialize_owl($('#owl1'));
+
+	let tabs = [
+		{ target: '#website', owl: '#owl1' },
+		{ target: '#uiux', owl: '#owl2' },
+	];
+
+	// Setup 'bs.tab' event listeners for each tab
+	tabs.forEach((tab) => {
+		$(`a[href="${tab.target}"]`)
+			.on('shown.bs.tab', () => initialize_owl($(tab.owl)))
+			.on('hide.bs.tab', () => destroy_owl($(tab.owl)));
+	});
+});
+
+function initialize_owl(el) {
+	el.owlCarousel({
+		loop: true,
+		autoplay: true,
+		autoplayTimeout: 3000,
+		dots: true,
+		nav: false,
+		responsive: {
+			0: {
+				items: 1,
+			},
+			600: {
+				items: 1,
+			},
+			1000: {
+				items: 2,
+			},
+		},
+	});
+}
+
+function destroy_owl(el) {
+	el.data('destroy');
+}
